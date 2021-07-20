b357169e26c88ddba8ee07a99bd3c15020c85e99
Lex Nelson <lexinelson597@gmail.com>
2021-07-20 17:27:43 -0400

    commit tree e7fecc659bf9ec22b466b77e04eabd2a444cd938

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,16 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {projects.map((project)=> {
+          console.log(project)
+          return <ProjectItem name ={project.name} 
+          key = {project.id} 
+          technologies = {project.technologies}
+          about = {project.about}
+          />
+        })}
+      </div>
     </div>
   );
 }
